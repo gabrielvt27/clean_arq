7978d6ba6668271d2a048b40d2b712cbca7c6ba2
Gabriel Valentin <tiburcio_gabriel@outlook.com>
2023-09-25 14:53:45 -0300

    Initialize repo

---
+++ test/data/usecases/remote_authentication_test.dart
@@ -0,0 +1,46 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({
+    required this.httpClient,
+    required this.url,
+  });
+
+  Future<void>? auth() async {
+    await httpClient.request(url: url, method: 'post');
+  }
+}
+
+abstract class HttpClient {
+  Future<void>? request({
+    required String url,
+    required String method,
+  });
+}
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  late RemoteAuthentication sut;
+  late HttpClient httpClient;
+  late String url;
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(
+      httpClient: httpClient,
+      url: url,
+    );
+  });
+
+  test('Should call HttpClient with correct URL and method', () async {
+    await sut.auth();
+    verify(httpClient.request(url: url, method: 'post'));
+  });
+}
